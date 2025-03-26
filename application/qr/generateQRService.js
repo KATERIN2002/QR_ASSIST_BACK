5087b311990da8e17cf06efd09dbe15907fa9966
KATERIN2002 <katerinmontes327@gmail.com>
2025-03-26 11:36:03 -0500

    cors

--- application/qr/generateQRService.js
+++ application/qr/generateQRService.js
@@ -3,15 +3,20 @@ const QRCode = require("qrcode");
 const generateQRService = (req, res) => {
   let qr = "";
 
-  QRCode.toString("https://gxxqktgc-3000.use.devtunnels.ms/", function (err, string) {
-    if (err) throw err;
-    qr = string;
-    console.log(string);
-  });
+  QRCode.toString(
+    "https://gxxqktgc-3000.use.devtunnels.ms/",
+    function (err, string) {
+      if (err) throw err;
+      qr = string;
+      console.log(string);
+    }
+  );
 
   console.log("┗|｀O′|┛┗|｀O′|┛");
 
-  res.status(200).json({ qrString: qr });
+  res
+    .status(200)
+    .json({ qrString: qr, url: "https://gxxqktgc-3000.use.devtunnels.ms/" });
 };
 
 module.exports = {
